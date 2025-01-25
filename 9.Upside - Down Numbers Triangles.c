b8ba049fa5c3a166ce5d5af0083562012a1b1872
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-25 02:42:54 -0800

    Create 9.Upside - Down Numbers Triangles.c

---
+++ 9.Upside - Down Numbers Triangles.c
@@ -0,0 +1,26 @@
+/* Formatted Numbers Upside - Down Triangles (lesson 9: Exercise 3) */
+#include <stdio.h>
+
+int main() {
+    int N=9;
+    int i,j,k;
+    
+    for (i=N; i>=1; i--) {
+        for (k=1; k<=3; k++) {
+            for (j=1; j<=i; j++) {
+                printf("%d", j);
+            }
+            for (j=1; j<=N-i; j++) {
+                printf(" ");
+            }
+            
+            for (j=1; j<=N-i; j++) {
+                printf(" ");
+            }
+            for (j=i; j>=1; j--) {
+                printf("%d", j);
+            }
+        }
+        printf("\n");
+    }
+}
